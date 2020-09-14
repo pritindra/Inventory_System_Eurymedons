68f41ec8e77cbf248879669fd69ceaaedec35fa3
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-14 23:03:00 +0530

    Update admin.py

--- interface/admin.py
+++ interface/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from interface.models import Arsenal
+
+# Register your models here.
+admin.site.register(Arsenal)
