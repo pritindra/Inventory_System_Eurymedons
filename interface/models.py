bab85c16d53a33ee7d003af648370076573f5743
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-14 23:01:21 +0530

    Update models.py

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+# Create your models here.
+class Arsenal(models.Model):
+    arsenal_type = models.CharField(max_length=20)
+    arsenal_name = models.CharField(max_length=30)
+    arsenal_number = models.IntegerField()
+    arsenal_maintenance = models.BooleanField()
+    arsenal_availability = models.TextField(max_length=1000)
+    image = models.ImageField(default='default.jpg', upload_to='arsenal_pics')
