d27c3472e3d1f6c5dee94ed02077f3c269712073
Aemitrius <67407608+Aemitrius@users.noreply.github.com>
2020-09-25 00:14:08 +0530

    Add files via upload

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -1,22 +1,14 @@
-"""Inventory URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from interface import views
+from item.views import ItemChartView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('interface.urls'))
+    path('', views.cover, name='cover'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('categories', views.categories, name='categories'),
+    path('check', views.check, name='check'),
+    path('arsenal', views.arsenal, name='arsenal'),
+   # path('', ItemChartView.as_view(), name='home'),
+    path('chart', ItemChartView.as_view(), name = 'chart')
+
 ]
---
+++ Inventory/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import sys
+
+# Create your views here.
+def cover(request):
+    return render(request, "interface/cover.html")
+
+def dashboard(request):
+    return render(request, "interface/dashboard.html")
+
+def categories(request):
+    return render(request, "interface/categories.html")
+
+def check(request):
+    return render(request, "interface/check.html")
+
+
+def arsenal(request):
+    return render(request, "interface/arsenal.html")
+
+def chart(request):
+    return render(request,"chart.html")
