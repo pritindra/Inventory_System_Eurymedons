ef65a0822b14477c84b90daf90a1395b10f52193
Aemitrius <67407608+Aemitrius@users.noreply.github.com>
2020-09-25 22:52:03 +0530

    Add files via upload

---
+++ home/models.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.db import models
+
+class AllData(models.Model):
+    finder = models.CharField(max_length=50)
+    encText = models.TextField(blank=True, null=True)
+    keys = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.finder
