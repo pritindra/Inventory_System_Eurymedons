5e328cbc7bcf0a2e40b84c70e742f1a2a2b08fc2
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-27 15:59:41 +0530

    Add files via upload

---
+++ encryption/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import AllData
+
+
+class EncryptForm(forms.Form):
+    decText = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'materialize-textarea',
+        'id': 'textarea2',
+        'rows': '5',
+        'data-length': '5000',
+    }))
+    finder = forms.CharField(widget=forms.TextInput(attrs={
+        'type': 'password',
+        'id': 'password',
+        'class': 'validate'
+    }))
+
+class DecryptForm(forms.Form):
+    encText = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'materialize-textarea',
+        'id': 'textarea2',
+        'rows': '5',
+        'data-length': '5000',
+    }))
+    finder = forms.CharField(widget=forms.TextInput(attrs={
+        'type': 'password',
+        'id': 'password',
+        'class': 'validate'
+    }))
