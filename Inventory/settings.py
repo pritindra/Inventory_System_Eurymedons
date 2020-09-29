297002aa6aa53eedd868ceaed6b976345b04991d
Aemitrius <67407608+Aemitrius@users.noreply.github.com>
2020-09-29 19:16:37 +0530

    Update settings.py

--- Inventory/settings.py
+++ Inventory/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'questionaire.apps.QuestionaireConfig',
+    'chat.apps.ChatConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
