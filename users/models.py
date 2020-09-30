5781996b1ca8337dee1b6c7e0b997cb3888fa2ab
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-30 20:07:55 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from PIL import Image
 
 
 # Create your models here.
@@ -14,6 +15,7 @@ class UserInfo(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
     mobile_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='user_pics')
 
 class Activity(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
