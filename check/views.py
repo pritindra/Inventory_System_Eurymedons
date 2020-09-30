0fabc972482b8aa52a7711dcb65aa331ee1585f8
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-30 09:09:19 +0530

    Add files via upload

--- check/views.py
+++ check/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from check.models import ItemHistory
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
+@login_required
 def checkin(request):
     return render(request, "check/checkin.html")
 
-class HistoryView(ListView):
+class HistoryView(ListView,LoginRequiredMixin):
     model = ItemHistory
     template_name = 'check/history.html'
     context_object_name = 'history'
