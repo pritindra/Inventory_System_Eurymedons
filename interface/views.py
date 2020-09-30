655f354264d354293983a54dce2ce5af706cd24b
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-30 09:11:34 +0530

    Add files via upload

--- interface/views.py
+++ interface/views.py
@@ -3,23 +3,27 @@ from interface.models import Arsenal, Vehicles, Criminal
 import sys
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, DeleteView,TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def cover(request):
     return render(request, "interface/cover.html")
 
+@login_required
 def contact(request):
     return render(request, "interface/contact.html")
 
-
+@login_required
 def categories(request):
     return render(request, "interface/categories.html")
 
+@login_required
 def check(request):
     return render(request, "interface/check.html")
 
-class ArsenalView(ListView):
+class ArsenalView(ListView,LoginRequiredMixin):
     model = Arsenal
     template_name = 'interface/arsenal.html'
     context_object_name = 'arsenals'
@@ -29,7 +33,7 @@ class ArsenalView(ListView):
         return data
 
 
-class VehiclesView(ListView):
+class VehiclesView(ListView,LoginRequiredMixin):
     model = Vehicles
     template_name = 'interface/vehicles.html'
     context_object_name = 'vehicles'
@@ -38,7 +42,7 @@ class VehiclesView(ListView):
         data = super().get_context_data(**kwargs)
         return data
 
-class CriminalView(ListView):
+class CriminalView(ListView,LoginRequiredMixin):
     model = Criminal
     template_name = 'interface/criminal.html'
     context_object_name = 'criminals'
@@ -47,7 +51,7 @@ class CriminalView(ListView):
         data = super().get_context_data(**kwargs)
         return data
 
-class ArsenalMoreInfo(DetailView):
+class ArsenalMoreInfo(DetailView,LoginRequiredMixin):
     model = Arsenal
     template_name = 'interface/more_info.html'
     context_object_name = 'arsenal'
@@ -55,17 +59,17 @@ class ArsenalMoreInfo(DetailView):
         data = super().get_context_data(**kwargs)
         return data
 
-class VehicleMoreInfo(DetailView):
+class VehicleMoreInfo(DetailView,LoginRequiredMixin):
     model = Vehicles
     template_name = 'interface/more_info_vehicles.html'
     context_object_name = 'vehicle'
 
-class CriminalMoreInfo(DetailView):
+class CriminalMoreInfo(DetailView,LoginRequiredMixin):
     model = Criminal
     template_name = 'interface/more_info_criminals.html'
     context_object_name = 'criminal'
     
-
+@login_required
 def help(request):
     return render(request, "interface/help.html")
 
