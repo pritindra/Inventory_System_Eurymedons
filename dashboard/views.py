ee8831fee2b6ba68d2347be7145972fcbe40652d
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-09-30 09:10:18 +0530

    Add files via upload

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Item
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class ItemChartView(TemplateView):
+class ItemChartView(TemplateView,LoginRequiredMixin):
     template_name='dashboard/dashboard.html'
 
     def get_context_data(self, **kwargs):
         context =super().get_context_data(**kwargs)
         context["qs"]= Item.objects.all()
         return context
+    
